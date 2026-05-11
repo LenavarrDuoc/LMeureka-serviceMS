240c39e8a8b9ad8df976cd9dafb9fda429446124
LenavarrDuoc <leo.navarr@duocuc.cl>
2026-05-11 02:30:08 -0400

    Todo configurado.

--- src/main/java/cl/duoc/eurekaservice/EurekaServiceApplication.java
+++ src/main/java/cl/duoc/eurekaservice/EurekaServiceApplication.java
@@ -2,12 +2,14 @@ package cl.duoc.eurekaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer //Viene incluida por defecto la anotación en Spring Boot moderno, pero se recomienda explicitarla para tener claro lo que estamos utilizando.
 @SpringBootApplication
 public class EurekaServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServiceApplication.class, args);
+    }
 
 }
